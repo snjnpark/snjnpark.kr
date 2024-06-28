0fd5a2829e8d427fe140f32d10258995ec210f95
photobymag <hello@snjnpark.kr>
2024-06-28 15:12:04 +0900

    ver 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
+  site: "https://snjnpark.kr",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
-      theme: "css-variables",
-    },
+      theme: "css-variables"
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
